1ebd19cdd3007883b53829992e898caf372de735
Luis Gonzalo Guzman Sanchez <105834318+GUSL-Luis-Gonzalo@users.noreply.github.com>
2022-05-18 23:12:03 -0500

    Update prog2.cpp

--- prog2.cpp
+++ prog2.cpp
@@ -1,5 +1,5 @@
 
-/* Autor: Andriks Imanol Ruiz Martinez fecha:03/02/2022
+/* Autor: Luis Gonzalo Guzman Sanchez fecha:03/02/2022
 Escuela: Universidad del Valle de Mexico Campus Villahermosa
 Materia: Programación Estructurada
 Ciclo: 01/2022
